a5c7249df6dc92e4eac37bfbd9a868724706a686
Meds <mahdi243@gmail.com>
2026-08-25 18:59:52 +0200

    chore: cleanup temp blog files

--- merge-blogs.js
+++
@@ -1,2 +0,0 @@
-const fs = require('fs');
-const oldData = fs.readFileSync('src/lib/blog-data-full.ts', 'utf8'); // Wait I ruined blog-data.ts
--- src/lib/blog-data-full.ts
+++
@@ -1,4 +0,0 @@
-import data2 from './blog-data-2.json';
-import { BLOG_POSTS as data1 } from './blog-data';
-
-export const ALL_BLOG_POSTS = [...data1, ...data2];
