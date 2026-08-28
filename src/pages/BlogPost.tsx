909e5bf42e3a85c93093b985588ec1633fdaf2d4
Meds <mahdi243@gmail.com>
2026-08-28 16:31:23 +0200

    feat(comments): activation Giscus avec les vrais IDs

--- src/pages/BlogPost.tsx
+++ src/pages/BlogPost.tsx
@@ -12,15 +12,15 @@ function GiscusComments() {
     const script = document.createElement('script');
     script.src = 'https://giscus.app/client.js';
     script.setAttribute('data-repo', 'warezfr/afbtaxi');
-    script.setAttribute('data-repo-id', 'YOUR_REPO_ID');
+    script.setAttribute('data-repo-id', 'R_kgDOUB_c-w');
     script.setAttribute('data-category', 'General');
-    script.setAttribute('data-category-id', 'YOUR_CATEGORY_ID');
+    script.setAttribute('data-category-id', 'DIC_kwDOUB_c-84DEYlu');
     script.setAttribute('data-mapping', 'pathname');
     script.setAttribute('data-strict', '0');
     script.setAttribute('data-reactions-enabled', '1');
     script.setAttribute('data-emit-metadata', '0');
     script.setAttribute('data-input-position', 'bottom');
-    script.setAttribute('data-theme', 'dark');
+    script.setAttribute('data-theme', 'preferred_color_scheme');
     script.setAttribute('data-lang', 'fr');
     script.setAttribute('crossorigin', 'anonymous');
     script.async = true;
